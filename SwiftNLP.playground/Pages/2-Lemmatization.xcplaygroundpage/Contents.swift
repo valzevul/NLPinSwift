c39aeff56bccc59457d79656418d0654300c5c10
Vadim Drobinin <vadim@drobinin.com>
2019-09-04 15:29:37 +0100

    Fix copies

--- SwiftNLP.playground/Pages/2-Lemmatization.xcplaygroundpage/Contents.swift
+++ SwiftNLP.playground/Pages/2-Lemmatization.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ func lemmatization(for text: String) {
   let range = text.startIndex..<text.endIndex
   tagger.enumerateTags(in: range, unit: .word, scheme: .lemma,
                        options: options) { (tag, range) -> Bool in
-                        if let lemma = tag?.rawValue, lemma.lowercased() != text[range].lowercased() {
+                        if let lemma = tag?.rawValue {
                           print("\(text[range]) -> \(lemma)")
                         }
                         return true
@@ -19,10 +19,7 @@ func lemmatization(for text: String) {
 
 // http://famouspoetsandpoems.com/poets/roald_dahl/poems/7799
 let quote = """
-The most important thing we've learned,
-So far as children are concerned,
-Is never, NEVER, NEVER let
-Them near your television set
+ Silver coins were minted at Aberystwyth Castle.
 """
 
 lemmatization(for: quote)
